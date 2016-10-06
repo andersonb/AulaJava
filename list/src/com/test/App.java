3e33bece3acda6cf293754eebff7ebf4fadf5c4d
Anderson Barbosa <maninho08@gmail.com>
2016-10-06 00:31:55 -0300

    first version of list

--- list/src/com/test/App.java
+++ list/src/com/test/App.java
@@ -5,5 +5,32 @@ public class App{
 	public static void main(String[] args){
 		
 		List lista = new List();
+		
+		lista.add(1);
+		lista.add(55);
+		lista.add(3);
+		lista.add(4);
+		
+		lista.print();
+		System.out.println("size " + lista.getSize()); 	
+		
+		System.out.println("get1 " + lista.get(0)); 	
+		System.out.println("get1 " + lista.get(1)); 	
+		System.out.println("get1 " + lista.get(2)); 	
+		System.out.println("get1 " + lista.get(3)); 	
+		//System.out.println("get1 " + lista.get(4)); 
+
+
+		lista.remove(0);
+		lista.print();
+		lista.remove(0);
+		lista.print();
+		lista.remove(0);
+		lista.print();
+		lista.remove(0);
+		lista.print();
+		
+		lista.remove(0);
+		lista.print();
 	}
 }
--- list/src/com/test/List.java
+++ list/src/com/test/List.java
@@ -1,5 +1,67 @@
 package com.test;
 
 public class List{
+	
+	private No start;
+	private int size = 0;
+	
+	public List(){
+		this.start = null;
+	}
+	
+	public void add(int value){
+		No insert = new No(value,null);
+		if(start == null){
+			start = insert;
+		}else{
+			No e = start;
+			while(e.getNext() != null){
+				e = e.getNext();
+			}
+			e.setNext(insert);
+		}
+		size++;
+	}
+	
+	public void print(){
+		No e = start;
+		while(e != null){
+			System.out.print(e.toString() + ",");
+			e = e.getNext();
+		}
+		System.out.println("\n");
+	}
+	
+	
+	public int get(int index){
+		if(index < 0 || index >= size) throw new RuntimeException("Index: " + index +" size:" + size);
+		No e = start;
+		for(int i = 0; i < index; i++){
+			e = e.getNext();
+		}
+		return e.getValue();
+	}
+	
+	
+	public void remove(int index){
+		if(index < 0 || index >= size) throw new RuntimeException("Index: " + index +" size:" + size);
+		No remove = start;
+		No ant    = start;
+		
+		for(int i = 0; i < index; i++){
+			ant = remove;
+			remove = remove.getNext();
+		}
+		if(index != 0){
+			ant.setNext(remove.getNext());
+		}else{
+			start = start.getNext();
+		}
+		size--;
+	}
+	
+	public int getSize(){
+		return size;
+	}
 
 }
